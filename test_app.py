0dc640af3f0fac3a5ae59ddfab46db64ea9a5d4d
adrianalucia2008 <tu_email@ejemplo.com>
2026-08-26 17:07:29 -0500

    test: agregar test funcional a ruta / y restaurar imagen base temporalmente

--- test_app.py
+++ test_app.py
@@ -1,2 +1,9 @@
-def test_ejemplo(): 
+from sample_app import app
+
+def test_ejemplo():
     assert 1 + 1 == 2  # nosec B101
+
+def test_home_status_code():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200

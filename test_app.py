4242ec13b7fec8891f372dba8707b9b87fcaf958
adrianalucia2008 <tu_email@ejemplo.com>
2026-08-27 22:13:11 -0500

    comprovacion2

--- test_app.py
+++ test_app.py
@@ -14,6 +14,6 @@ def test_home_status_code(client):
     assert response.status_code == 200
 
 def test_version_endpoint(client):
-    """Verifica que la ruta /version responda con un código HTTP 200 Ok."""
+    """Verifica que la ruta /version responda con un código HTTP 200 OK."""
     response = client.get('/version')
     assert response.status_code == 200
